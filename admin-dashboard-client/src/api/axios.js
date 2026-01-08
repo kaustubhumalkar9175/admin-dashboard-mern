916259ee2f3e439c10f052db9056f3d3d5b641a4
Kaustubh Umalkar <kaustubhumalkar9175@gmail.com>
2026-01-08 10:55:05 +0530

    Fix Netlify routing using _redirects

--- admin-dashboard-client/src/api/axios.js
+++ admin-dashboard-client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://admin-dashboard-mern-ju01.onrender.com/api",
 });
 
 // Attach token automatically
