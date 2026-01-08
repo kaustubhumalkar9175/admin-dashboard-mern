d4c32e044983e0503f8d0eeeb53c2dc4d1fb0a5e
Kaustubh Umalkar <kaustubhumalkar9175@gmail.com>
2026-01-08 10:49:56 +0530

    temp

--- admin-dashboard-client/src/api/axios.js
+++ admin-dashboard-client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://admin-dashboard-mern-ju01.onrender.com/api",
+  baseURL: "http://localhost:5000/api",
 });
 
 // Attach token automatically
